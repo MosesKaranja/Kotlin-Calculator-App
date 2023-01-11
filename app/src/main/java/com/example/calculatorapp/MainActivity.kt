d43efe1a3d2636dc738acaf4fbc27e796d1047a5
unknown <moses.karanja@strathmore.edu>
2023-01-11 15:52:53 +0300

    calc app changes

--- app/src/main/java/com/example/calculatorapp/MainActivity.kt
+++ app/src/main/java/com/example/calculatorapp/MainActivity.kt
@@ -2,11 +2,131 @@ package com.example.calculatorapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    var isNewOp:Boolean = true
+    var oldNumber = ""
+    var op = "+"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun numberEvent(view: View){
+        val editText = findViewById<EditText>(R.id.editText)
+
+        if (isNewOp){
+            editText.setText("")
+
+        }
+        isNewOp = false
+        var buclick:String = editText.text.toString()
+        var buselect:Button = view as Button
+
+        when(buselect.id){
+            bu1.id-> {
+                buclick += "1"
+            }
+            bu2.id->{
+                buclick += "2"
+            }
+            bu3.id->{
+                buclick += "3"
+            }
+            bu4.id->{
+                buclick += "4"
+            }
+            bu5.id->{
+                buclick += "5"
+            }
+            bu6.id->{
+                buclick += "6"
+            }
+            bu7.id->{
+                buclick += "7"
+            }
+            bu8.id->{
+                buclick += "8"
+            }
+            bu9.id->{
+                buclick += "9"
+            }
+            bu0.id->{
+                buclick += "0"
+            }
+            buDot.id->{
+                buclick += "."
+            }
+            buPlusMinus.id->{
+                buclick= "-$buclick"
+            }
+
+        }
+        editText.setText(buclick)
+
+    }
+
+    fun operatorEvent(view: View){
+        isNewOp = true
+        oldNumber = editText.text.toString()
+
+        var buselect: Button = view as Button
+        when(buselect.id){
+            buMultiply.id->{
+                op = "*"
+
+            }
+            buPlus.id->{
+                op = "+"
+
+            }
+            buMinus.id->{
+                op = "-"
+
+            }
+            buDivide.id->{
+                op="/"
+
+            }
+        }
+    }
+
+    fun equalEvent(view: View){
+        var newNumber:String = editText.text.toString()
+        var result = 0.0
+
+        when(op){
+            "+"->{
+                result = oldNumber.toDouble() + newNumber.toDouble()
+            }
+            "*"->{
+                result = oldNumber.toDouble() * newNumber.toDouble()
+            }
+            "/"->{
+                result = oldNumber.toDouble() / newNumber.toDouble()
+            }
+            "-"->{
+                result = oldNumber.toDouble() - newNumber.toDouble()
+            }
+        }
+        editText.setText(result.toString())
+    }
+
+    fun acEvent(view: View){
+        editText.setText("0")
+        isNewOp=true
+
+    }
+
+    fun percentageEvent(view: View){
+        var no:Double = editText.text.toString().toDouble() / 100
+        editText.setText(no.toString())
+
+    }
+
 }
